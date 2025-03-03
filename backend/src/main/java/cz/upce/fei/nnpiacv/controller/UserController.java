d0377a6dc8919a0ed52041aa8ffd63be9b515143
Palkitch <125451425+Palkitch@users.noreply.github.com>
2025-03-03 14:21:18 +0100

    CV-03 + CV-04

--- backend/src/main/java/cz/upce/fei/nnpiacv/controller/UserController.java
+++ backend/src/main/java/cz/upce/fei/nnpiacv/controller/UserController.java
@@ -1,36 +1,49 @@
 package cz.upce.fei.nnpiacv.controller;
 
 import cz.upce.fei.nnpiacv.domain.User;
+import cz.upce.fei.nnpiacv.dto.UserRequestDto;
+import cz.upce.fei.nnpiacv.dto.UserResponseDto;
 import cz.upce.fei.nnpiacv.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Optional;
 
 @RestController
+@AllArgsConstructor
 public class UserController {
-    private UserService userService;
+    private final UserService userService;
 
-    @Autowired
-    public UserController(UserService greetingService) {
-        this.userService = greetingService;
-    }
-
-    @GetMapping("/user")
-    public User findUserById(@RequestParam Long id) {
-        return userService.findUserById(id);
-    }
     @GetMapping("/user/{id}")
-    public User findUserByPath(@PathVariable Long id) {
-        return userService.findUserById(id);
+    public User findUser(@PathVariable("id") Long id) {
+        return userService.findUser(id);
     }
+
+
+    @PostMapping("api/v1/users")
+    public ResponseEntity<?> createUser(@RequestBody UserRequestDto user) {
+        User createdUser = userService.createUser(new User(user.getEmail(), user.getPassword()));
+        /*return ResponseEntity.status(201).body(
+                new UserResponseDto(createdUser.getId(), createdUser.getEmail(), createdUser.getPassword()));
+        */
+        return ResponseEntity.status(201).body(
+                UserResponseDto.builder().id(createdUser.getId()).password(createdUser.getPassword()).email(createdUser.getEmail()).build());
+    }
+
     @GetMapping("/users")
-    public Collection<User> findUsers() {
-        return userService.findUsers();
+    public Collection<? extends Object> findUsers(@RequestParam(required = false) String email) {
+        if (email != null) {
+            return userService.findUsers();
+        } else {
+            Optional<User> user = userService.findbyEmail(email);
+            if (user == null) {
+                return Collections.emptyList();
+            } else {
+                return Collections.singletonList(user);
+            }
+        }
     }
-
-
 }
